9999bb6f2fc7911e93e730b5be4af494785157b7
Alex4790354 <9720560@gmail.com>
2025-04-21 18:33:07 +0300

    Initial commit.

---
+++ src/main/java/com/pokemons/png/PngApplication.java
@@ -0,0 +1,13 @@
+package com.pokemons.png;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PngApplication.class, args);
+	}
+
+}
