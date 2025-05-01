c7e9b72ba4b61e9030ddcae4b7b8cfdf40d37b58
Alex4790354 <9720560@gmail.com>
2025-05-01 14:55:05 +0300

    Version-5.

--- src/main/java/com/pokemons/png/service/MainLogic.java
+++ src/main/java/com/pokemons/png/service/MainLogic.java
@@ -5,6 +5,8 @@ import com.pokemons.png.request.PokemonShort;
 import com.pokemons.png.request.NewPokemon;
 import com.pokemons.png.request.Pokemon;
 import com.pokemons.png.response.ResponseMessage;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -19,7 +21,8 @@ import java.util.Comparator;
 @Component
 public class MainLogic {
 
-    private int count = 875; //872
+    private int CURRENT_COUNT = 41; //872
+    private int MAX_TRAINER_LEVEL = 6; // min:2, max:6
     private final SendRequest sendRequest;
     private final List<Pokemon> pokemonNames = Arrays.asList(
         /*new Pokemon("JSON", 311),
@@ -32,7 +35,8 @@ public class MainLogic {
     private final String MY_TRAINER_ID = "36046";
     private boolean isLastBattle = false;
     private int sleepCount = 0;
-    private int sleepTime = 61_000;
+    private int sleepTime = 5_000;
+    private boolean isLeaveInPokeBall = true;
     String POKEMON_INTO_BALL_URL = "HTTPS://api.pokemonbattle.ru/v2/trainers/add_pokeball";
     String POKEMON_FROM_BALL_URL = "HTTPS://api.pokemonbattle.ru/v2/trainers/delete_pokeball";
 
@@ -53,6 +57,8 @@ public class MainLogic {
         PokemonShort myPokemonShort = new PokemonShort("0");
         String enemyPokemonId;
         ResponseMessage responseMessage;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        String currentTime;
 
         //for (int i = 0; i < 10; i++) {
         while (true) {
@@ -60,11 +66,15 @@ public class MainLogic {
             enemyPokemonId = "0";
             myPokemonId = "0";
             getPokemonsList();
+            if (myPokemons == null) {
+                myPokemons = new ArrayList<>();
+            }
 
             if (myPokemons.size() < 3) {
                 for (int i = myPokemons.size(); i < 3; i++) {
                     myPokemonId = createPokemon(myPokemons);
-                    System.out.println("##64 New pokemon with ID = " + myPokemonId + " created");
+                    currentTime = LocalTime.now().format(formatter);
+                    System.out.println("##68 " + currentTime + " New pokemon with ID = " + myPokemonId + " created");
                     getPokemonsList();
                     myPokemonId = "0";
                 }
@@ -81,45 +91,56 @@ public class MainLogic {
 
             if (enemyPokemonId.equals("0")) {
                 if (sleepCount <= 1) {
-                    System.out.println("##80. No enemy found");
+                    currentTime = LocalTime.now().format(formatter);
+                    System.out.println("##95 " + currentTime + " . No enemy found");
                 } else if (sleepCount > 60) {
                     sleepCount = -1;
                     isLastBattle = false;
                     sleepTime = 60_000;
                 }
                 sleepCount++;
+                sleepTime = 305_000;
             } else if (myPokemonId.equals("0")) {
-                System.out.println("##88. My pokemonId = 0");
+                currentTime = LocalTime.now().format(formatter);
+                System.out.println("##105 " + currentTime + " .My pokemonId = 0");
             } else if (isLastBattle) {
                 if (sleepCount == 0) {
-                    System.out.println("##91. The attack limit has been reached for today. Sleep tight");
+                    currentTime = LocalTime.now().format(formatter);
+                    System.out.println("##109 " + currentTime + " . The attack limit has been reached for today. Sleep tight");
                 } else if (sleepCount > 60) {
                     sleepCount = -1;
                     isLastBattle = false;
-                    sleepTime = 60_000;
                 }
                 sleepCount++;
+                sleepTime = 305_000;
             }  else {
-
+                sleepTime = 5_000;
                 if (myPokemon.getInPokeball() != 1) {
                     responseMessage = sendRequest.makeRequest(myPokemonShort.toString(), POKEMON_INTO_BALL_URL, HttpMethod.POST);
-                    System.out.println("##101. Gettging ready for fight. message.getStatus(): " + responseMessage.getStatus());
+                    currentTime = LocalTime.now().format(formatter);
+                    System.out.println("##121 " + currentTime + " . Gettging ready for fight. message.getStatus(): " + responseMessage.getStatus());
                 }
 
                 responseMessage = fight(new Battle(myPokemonId, enemyPokemonId));
                 if (responseMessage.getMessage().contains("Твой лимит боёв исчерпан")) {
-                    System.out.println("##107. The battle limit has been reached");
+                    currentTime = LocalTime.now().format(formatter);
+                    System.out.println("##127 " + currentTime + " . The battle limit has been reached");
                     isLastBattle = true;
                     sleepTime = 300_000;
                 } else {
-                    System.out.println("##111. Selected pokemon for attack: " + enemyPokemonId);
-                    System.out.println(
-                        "##113. The fight has passed. getMessage(): " + responseMessage.getMessage() + ", \n .getResult(): " +
+                    currentTime = LocalTime.now().format(formatter);
+                    System.out.println("##132 " + currentTime + " . Selected pokemon for attack: " + enemyPokemonId);
+                    System.out.println("##133 " + currentTime + " . The fight has passed. getMessage(): " + responseMessage.getMessage() + ", .getResult(): " +
                             responseMessage.getResult() + ", " + responseMessage.getBattleLimit());
 
                     if (responseMessage.getResult().contains("победил")) {
-                        responseMessage = sendRequest.makeRequest(myPokemonShort.toString(), POKEMON_FROM_BALL_URL, HttpMethod.PUT);
-                        System.out.println("##118. Вытаскиваю покемона: " + responseMessage.getMessage());
+                        if (!isLeaveInPokeBall) {
+                            responseMessage = sendRequest.makeRequest(myPokemonShort.toString(), POKEMON_FROM_BALL_URL,
+                                HttpMethod.PUT);
+                            System.out.println("        ##140 " + currentTime + " . Вытаскиваю покемона: " + responseMessage.getMessage());
+                        } else {
+                            System.out.println("        ##141 " + currentTime + " . Оставил в покеболе: " + responseMessage.getMessage());
+                        }
                     } else if (responseMessage.getResult().contains("проиграл")) {
                         myPokemons.remove(myPokemon);
                         createPokemon(myPokemons);
@@ -145,10 +166,15 @@ public class MainLogic {
                 .anyMatch(pokemon -> pokemon.getPhotoId() == pokemonTemplate.getPhotoId());
 
             if (!exists) {
-                NewPokemon pokemon = new NewPokemon(pokemonTemplate.getName() + count++, pokemonTemplate.getPhotoId());
+                NewPokemon pokemon = new NewPokemon(pokemonTemplate.getName() + CURRENT_COUNT++, pokemonTemplate.getPhotoId());
                 String CREATE_POKEMON_URL = "HTTPS://api.pokemonbattle.ru/v2/pokemons";
                 ResponseMessage message = sendRequest.makeRequest(pokemon.toString(), CREATE_POKEMON_URL, HttpMethod.POST);
-                PokemonShort myPokemon = new PokemonShort("0");
+
+                if (isLeaveInPokeBall) {
+                    PokemonShort pokemonShort = new PokemonShort(message.getId());
+                    message = sendRequest.makeRequest(pokemonShort.toString(), POKEMON_INTO_BALL_URL, HttpMethod.POST);
+                }
+
                 return message.getId();
             }
         }
@@ -180,7 +206,7 @@ public class MainLogic {
             Thread.sleep(100); // 60_000 milliseconds = 1 min
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
-            System.out.println("Thread was interrupted, failed to complete sleep");
+            System.out.println("##194. Thread was interrupted, failed to complete sleep");
         }
         myPokemons = sendRequest.makeRequest("", MY_POKEMON_LIST_URL, HttpMethod.GET).getData();
     }
@@ -200,7 +226,7 @@ public class MainLogic {
 
         return enemyPokemons.stream()
             .filter(pokemon -> pokemon.getAttack() == 1)
-            .filter(pokemon -> trainersMap.get(pokemon.getTrainerId()) != null && trainersMap.get(pokemon.getTrainerId()) == 1)
+            .filter(pokemon -> trainersMap.get(pokemon.getTrainerId()) != null && trainersMap.get(pokemon.getTrainerId()) < MAX_TRAINER_LEVEL)
             .findFirst()
             .map(Pokemon::getId)
             .orElse("0");
